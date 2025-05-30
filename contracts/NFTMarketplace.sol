bf2eb7358a8308d1a8b1570fdebf23c604930bff
SHORYA BAJ <jain.shorya11@gmail.com>
2025-05-31 00:14:29 +0530

    nft contract checkpoit 1

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -4,25 +4,40 @@ pragma solidity ^0.8.19;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "hardhat/console.sol";
+import "../contracts/Counter.sol";
 
 contract NFTMarketplace is ERC721URIStorage {
-    uint256 public tokenCounter;
-    address public owner;
+    using Counter for Counter.Counter;
 
-    event NFTCreated(uint256 indexed tokenId, string tokenURI, address owner);
+    Counter.Counter private _tokenIdCounter;
+    Counter.Counter private _itemsSold;
 
-    constructor() ERC721("NFTMarketplace", "NFTM") {
-        tokenCounter = 0;
-        owner = msg.sender;
+    uint256 listingPrice= 0.025 ether;
+    address payable owner;
+    mapping(uint256 => MarketItem) private idToMarketItem;
+    struct MarketItem {
+        uint256 tokenId;
+        address payable seller;
+        address payable owner;
+        uint256 price;
+        bool sold;
     }
-
-    function createNFT(string memory tokenURI) public returns (uint256) {
-        uint256 newItemId = tokenCounter;
-        _safeMint(msg.sender, newItemId);
-        _setTokenURI(newItemId, tokenURI);
-        emit NFTCreated(newItemId, tokenURI, msg.sender);
-        tokenCounter++;
-        return newItemId;
+    event MarketItemCreated(
+        uint256 indexed tokenId,
+        address  seller,
+        address  owner,
+        uint256 price,
+        bool sold
+    );
+    constructor() ERC721("NFTMarketplace", "NFTM") {
+        owner = payable(msg.sender);
+    }
+    function udateListingPrice(uint256 _listingPrice) public payable{
+        require(owner == msg.sender, "Only owner can update the listing price");
+        listingPrice = _listingPrice;
+    }
+    function getListingPrice() public view returns (uint256) {
+        return listingPrice;
     }
     
 }
