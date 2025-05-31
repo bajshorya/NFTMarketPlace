64ad22c7a972118b13a77d47cf9c7a426bad491b
SHORYA BAJ <jain.shorya11@gmail.com>
2025-05-31 20:55:23 +0530

    deleted dumbass hardhat files , which destroyed my last 2 hrs

---
+++ context/constants.ts
@@ -0,0 +1,4 @@
+//0x2eE81838bA588FDCBcE1508E7F3E5f5d149d7eaB
+import market from "./NFTMarketplace.json";
+export const marketAddress = "0x2eE81838bA588FDCBcE1508E7F3E5f5d149d7eaB";
+export const marketABI = market.abi;
