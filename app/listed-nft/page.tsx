69d81cb51404be54d07c66bdd171eee345ecb4c4
SHORYA BAJ <jain.shorya11@gmail.com>
2025-06-04 02:16:52 +0530

    anothe rmargin change

--- app/listed-nft/page.tsx
+++ app/listed-nft/page.tsx
@@ -39,7 +39,7 @@ const Page = () => {
   }
 
   return (
-    <div className=" min-h-screen py-12 px-4 sm:px-6 lg:px-8">
+    <div className=" min-h-screen px-4 sm:px-6 lg:px-8">
       <div className="max-w-7xl mx-auto">
         <Banner
           name="Listed NFT's for Sale"
