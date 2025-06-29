8e477c75033a6b3f16c147cb67e8813c2c6713aa
SHORYA BAJ <jain.shorya11@gmail.com>
2025-06-29 23:35:37 +0530

    added adspace

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,18 @@
 "use client";
-// import Banner from "@/components/Banner";
 import ScrollCard from "@/components/CreatorCard";
 import HotBids from "@/components/HotBids";
-// import AdSpaceProvider from "adspace-provider";
+import AdSpaceProvider from "adspace-provider";
 export default function Home() {
   return (
     <div className="px-10">
       <div>
         <div className="w-full">
-          {/* <Banner
-            name="Discover Digital Art"
-            parentStyles="h-[70vh] min-h-[500px] rounded-3xl p-8"
-            childStyles="drop-shadow-lg"
-          /> */}
-          {/* <div>
+          <div>
             <AdSpaceProvider
-              tokenId={0}
-              contractAddress="0x1C1B73B1D9b4eF7775b30C0301fdE00615C17682"
+              tokenId={1}
+              contractAddress="0xd9A04562B2ea577007097B013e625F0CB2Fbf83A"
             />
-          </div> */}
+          </div>
         </div>
         <div className="max-w-7xl mx-auto py-12">
           <ScrollCard />
